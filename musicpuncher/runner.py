6764742068955cdba31c457355366cddba4be48d
Bert Baron <bertusbaron@gmail.com>
2019-12-12 08:25:53 +0100

    Refactoring

--- musicpuncher/runner.py
+++ musicpuncher/runner.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 from mido import MidiFile
 
 from .keyboard import Keyboard
@@ -5,21 +7,25 @@ from .puncher_adapter import DebugAdapter
 
 MIN_VELOCITY = 20  # PPP
 
+class TimeNotes(object):
+    def __init__(self, delta: float, notes: List[int]):
+        self.delta = delta
+        self.notes = notes
+
+NoteList = List[TimeNotes]
 
 class MidiProcessor(object):
     @staticmethod
-    def __get_notes(filename: str):
+    def __get_notes(notes: NoteList):
         noteset = set()
-        with MidiFile(filename) as mid:
-            for msg in mid:
-                if (msg.type == 'note_on' or msg.type == 'note_off'):
-                    noteset.add(msg.note)
+        for tuple in notes:
+            noteset.update(tuple.notes)
         return noteset
 
-    def __init__(self, filename: str, adapter):
-        self.filename = filename
+    def __init__(self, notes: NoteList, adapter):
+        self.notes = notes
         self.adapter = adapter
-        noteset = MidiProcessor.__get_notes(filename)
+        noteset = MidiProcessor.__get_notes(notes)
         self.transposition = adapter.keyboard.calculate_transposition(noteset)
         print(f"Notes: {sorted(noteset)}")
         print(f"Transposing by {self.transposition}")
@@ -28,21 +34,14 @@ class MidiProcessor(object):
         self.adapter.reset()
         self.last_note = 0  # we remember last note so we can optimize the order
 
-        with MidiFile(self.filename) as mid:
-            notes_on = set()
-            delta = 0
-            for msg in mid:
-                if msg.type == 'note_on' and msg.velocity >= MIN_VELOCITY:
-                    notes_on.add(msg.note + self.transposition)
-                if (msg.type == 'note_on' or msg.type == 'note_off') and msg.time != 0:
-                    self.__punch(delta, notes_on)
-                    delta = msg.time
+        for tuple in self.notes:
+            self.__punch(tuple.delta, tuple.notes)
 
     def __punch(self, delta, notes):
         if len(notes) == 0:
             self.adapter.move(0, delta)
         else:
-            note_list = sorted(notes)
+            note_list = sorted([note + self.transposition for note in notes])
             if abs(self.last_note - note_list[0]) > abs(self.last_note - note_list[-1]):
                 note_list.reverse()
             for note in note_list:
@@ -51,11 +50,26 @@ class MidiProcessor(object):
                 self.last_note = note
                 delta = 0
 
+def __parse_midi(filename: str) -> NoteList:
+    """Returns a list of (timedelta, notelist) tuples"""
+    notes = []
+    with MidiFile(filename) as mid:
+        notes_on = set()
+        delta = 0
+        for msg in mid:
+            if msg.type == 'note_on' and msg.velocity >= MIN_VELOCITY:
+                notes_on.add(msg.note)
+            if (msg.type == 'note_on' or msg.type == 'note_off') and msg.time != 0:
+                notes.append(TimeNotes(delta, list(notes_on)))
+                delta = msg.time
+    return notes
+
 
 def run(args):
     #                    c   d   e   f   g   a   b   c   d   e   f   g   a   b   c
     keyboard = Keyboard([48, 50, 52, 53, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 72])
     adapter = DebugAdapter(keyboard)
-    processor = MidiProcessor(args[1], adapter)
+    notes = __parse_midi(args[1])
+    processor = MidiProcessor(notes, adapter)
 
     processor.process()
